50d131695e1dfca95db8c966864129b16b64fd5a
Sergey Plashenko <spl@sitecore.net>
2018-04-04 12:25:08 +0300

    Fix for 214979 was added

---
+++ src/Sitecore.Support.124871.198208/Analytics/Pipelines/SubmitSessionContext/SaveVisit.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Analytics.Configuration;
+using Sitecore.Analytics.Lookups;
+using Sitecore.Analytics.Pipelines.SubmitSessionContext;
+using Sitecore.Diagnostics;
+using Sitecore.Support.Analytics.Extensions;
+
+namespace Sitecore.Support.Analytics.Pipelines.SubmitSessionContext
+{
+  public class SaveVisit : Sitecore.Analytics.Pipelines.SubmitSessionContext.SaveVisit
+  {
+    public override void Process(SubmitSessionContextArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      var originalSession = args.Session.GetOriginalSession();
+      if (originalSession != null && originalSession.Interaction != null)
+      {
+        originalSession.Interaction.SaveDateTime = System.DateTime.UtcNow;
+        if (AnalyticsSettings.RedactIpAddress)
+        {
+          originalSession.Interaction.Ip = IpHashProviderBase.EmptyIpAddress;
+        }
+      }
+
+      base.Process(args);
+    }
+  }
+}
